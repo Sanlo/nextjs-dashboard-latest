bb8509f5ff96794b04f1cc0ae543c36a47762b38
Sanlo <nestorluther@gmail.com>
2026-02-09 18:15:30 +0800

    adding authentication for credential logins

---
+++ proxy.ts
@@ -0,0 +1,8 @@
+﻿import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+
+export default NextAuth(authConfig).auth;
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/imgae|.*\\.png$).*)'],
+};
